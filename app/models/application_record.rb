863da582f01d1727897ee06871951614a62509b1
thomashexton <tom.hexton@gmail.com>
2019-07-17 10:44:04 +1000

    added methods to remove timestamp columns from all  JSONs, unless specified

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,17 @@
 class ApplicationRecord < ActiveRecord::Base
-  self.abstract_class = true
+	self.abstract_class = true
+
+	# ignore created_at and updated_at by default in JSONs
+	# and when needed add them to :include
+	def serializable_hash(options={})
+		options[:except] ||= []
+		options[:except] << :created_at unless (options[:include] == :created_at) || (options[:include].kind_of?(Array) && (options[:include].include? :created_at))
+		options[:except] << :updated_at unless (options[:include] == :updated_at) || (options[:include].kind_of?(Array) && (options[:include].include? :updated_at))
+
+		options.delete(:include) if options[:include] == :created_at
+		options.delete(:include) if options[:include] == :updated_at
+		options[:include] -= [:created_at, :updated_at] if options[:include].kind_of?(Array)
+
+		super(options)
+	end
 end
