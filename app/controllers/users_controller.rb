087c3b3267681e760ef6f48dd68bb7accc4d81fb
thomashexton <tom.hexton@gmail.com>
2019-07-17 12:07:40 +1000

    updated json response

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,8 +18,7 @@ class UsersController < ApplicationController
 
 		user.save
 
-		name = user.first_name + " " + user.last_name
-		render :json => { user_id: user.id, name_submitted: name, consecutive_zeroes: consecutive_zeroes }
+		render :json => { id: user.id, first_name: user.first_name, last_name: user.last_name, ascii_total: user.ascii_total, binary_conversion: user.binary_conversion, consecutive_zeroes: consecutive_zeroes }
 	end
 
 	def show
